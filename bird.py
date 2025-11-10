7d8842fc17e13ea96dfbecdf5ef9727d4fae948b
taylaf001 <fonty005@mymail.unisa.edu.au>
2025-11-10 13:38:17 +1030

    cleaned up code in bird.py ensuring consistent snake case + programming standards

--- bird.py
+++ bird.py
@@ -9,25 +9,25 @@ This is my own work as defined by the University's Academic Integrity Policy.
 from animal import Animal
 
 class Bird(Animal):
-    def __init__(self, name, classification, species, age, dietary_requirements, specialisation_needed, preferred_environment, preferred_space, can_Fly):
+    def __init__(self, name, classification, species, age, dietary_requirements, specialisation_needed, preferred_environment, preferred_space, can_fly):
         # Animal class attributes
         super().__init__(name, classification, species, age, dietary_requirements, specialisation_needed, preferred_environment, preferred_space)
 
         # Empty Attributes which include data validation to ensure only valid inputs are passed in and set
-        self.__can_Fly = can_Fly
+        self.__can_fly = can_fly
 
-        if isinstance(can_Fly, bool):
-            self.__can_Fly = can_Fly
+        if isinstance(can_fly, bool):
+            self.__can_Fly = can_fly
         else:
             raise TypeError("Set 'Can fly' to either True or False")
 
     # Getter
-    def get_can_Fly(self):
-        return self.__can_Fly
+    def get_can_fly(self):
+        return self.__can_fly
 
     # Setter
-    def set_can_Fly(self, can_Fly):
-        self.__can_Fly = can_Fly
+    def set_can_fly(self, can_fly):
+        self.__can_Fly = can_fly
 
     # Methods including abstract ones from animal class
     def making_sounds(self):
@@ -40,7 +40,7 @@ class Bird(Animal):
         print(f"{self.get_name()} sleeps perched, high up in a tree... *zzzzz*")
 
     def fly(self):
-        if self.get_can_Fly() == True:
+        if self.__can_fly() == True:
             print(f"{self.get_name()} zooms through the blue sky.. *squark*")
         else:
             print(f"{self.get_name()} is unable to fly")
@@ -48,8 +48,8 @@ class Bird(Animal):
     def preen_feathers(self):
         print(f"{self.get_name()} carefully preens its feathers, smoothing and cleaning them..")
 
-    def build_Nest(self):
+    def build_nest(self):
         print(f"{self.get_name()} gathers nesting material and carefully creates a cozy nest.. *peck*")
 
-    def lay_Egg(self):
+    def lay_egg(self):
         print(f"{self.get_name()} lays an egg..")
